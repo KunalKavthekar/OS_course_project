e8d61486f0dd80b3ca8007504a325cb8c3c8ae43
sukhdeepg <gillsukhdeep452@gmail.com>
2019-03-23 14:34:43 +0530

    removed the wrap

--- phase1.py
+++ phase1.py
@@ -1,10 +1,10 @@
-from textwrap import wrap #To divide the instruction set into 4 equal parts
-
 input_text = open("input1.txt", "r")
 output_text = open("output1.txt", "w")
 section_divide = input_text.read().split('$AMJ') #Split the sections according to the common beginning($AMJ)
-prog = '$AMJ'+section_divide[1]
+prog = '$AMJ'+section_divide[2]
 # output_text.write(prog)
+
+print(prog)
 
 instr_set = []
 remove_nl = []
@@ -22,4 +22,15 @@ for x in remove_nl[16:]:
         instr_set.append(x)
 
 str_instr = ''.join(instr_set)
-print(wrap(str_instr, 4))
+
+data = ''.join(remove_nl[16:]).split('$DTA')[1].split('$')[0] #This will get the data inside the $DTA and $END section
+
+instr_data = str_instr + data
+
+instr_data_4 = [instr_data[i:i+4] for i in range(0, len(instr_data), 4)]  #To divide the instruction and data set into 4 equal parts
+
+print(instr_data_4)
+    
+
+
+
