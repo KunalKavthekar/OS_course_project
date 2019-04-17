18fdf1c22b4365965e7e5b272c7b06021e40e898
sukhdeepg <gillsukhdeep452@gmail.com>
2019-04-17 13:11:51 +0530

    divided in proper chunks

--- phase1.py
+++ phase1.py
@@ -12,7 +12,7 @@ input_text = open("input1.txt", "r")   #file read from full path
 # input_text = open("input1.txt", "r")
 output_text = open("output1.txt", "w")
 section_divide = input_text.read().split('$AMJ') #Split the sections according to the common beginning($AMJ)
-prog = '$AMJ'+section_divide[1] #changed from [2] to [1] as interpreter throws index error 
+prog = '$AMJ'+section_divide[2] #changed from [2] to [1] as interpreter throws index error 
 
 #Kunal, this will give you data before H
 i_need_till_H = []
@@ -21,18 +21,15 @@ for x in prog:
         break
     i_need_till_H.append(x)
 
+i_need_till_H = [x.replace('\n', '') for x in i_need_till_H]
 i_need_till_H = "".join(i_need_till_H)
-print(i_need_till_H)
 
 #This will give you data after H
 after_H = prog.split('$DTA')[1].split('$')[0]
-after_H = [x.replace('\n', '') for x in after_H]
 after_H = ''.join(after_H)
-print(after_H)
+after_H = after_H.split("\n")
     
-# output_text.write(prog)
-
-# print(section_divide[1])
+output_text.write(prog)
 
 instr_set = []
 remove_nl = []
@@ -49,24 +46,30 @@ for x in remove_nl[16:]:
     else:
         instr_set.append(x)
 
-
-
 str_instr = ''.join(instr_set)
+
+# print(str_instr)
 
 data = ''.join(remove_nl[16:]).split('$DTA')[1].split('$')[0] #This will get the data inside the $DTA and $END section
 
 # instr_data_4 = [instr_data[i:i+4] for i in range(0, len(instr_data), 4)]  #To divide the instruction and data set into 4 equal parts
 # creating seperate lists for instruction set and data.
 str_instr_4 = [str_instr[i:i+4] for i in range(0, len(str_instr), 4)]
-data_4 = [data[i:i+4] for i in range(0, len(data), 4)]
+# data_4 = [data[i:i+4] for i in range(0, len(data), 4)]
+str_instr = list(grouper(str_instr_4, 10))
+for x in after_H[1:-1]:
+    x = [x[i:i+4] for i in range(0, len(x), 4)]
+    x = list(grouper(x, 10))
+    str_instr.append(x)
+pp.pprint(str_instr)
 
 # using grouper function to create uniform chunks
-part_1 = list(grouper(str_instr_4, 10))
-part_2 = list(grouper(data_4, 10))
+# part_1 = list(grouper(str_instr_4, 10))
+# part_2 = list(grouper(data_4, 10))
 
 # instr_data_4 = str_instr_4 + data_4
 # puting them together to create final stack
-instr_data_4 = part_1 + part_2  
-pp.pprint(instr_data_4)
+# instr_data_4 = part_1 + part_2  
+# pp.pprint(instr_data_4)
 
 # execution of program will start here
