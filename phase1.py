749718482096c27e372d5699718b7160743a7db3
KunalKavthekar <42344635+KunalKavthekar@users.noreply.github.com>
2019-04-22 22:47:39 +0530

    Added imp functions and code cleaned

--- phase1.py
+++ phase1.py
@@ -1,55 +1,149 @@
 import pprint
 import itertools as it
 
+buffer = ""
+R = ""
+C = 0
+output = ""
+SI = 0
+card = 0
+
 def grouper(iterable, n, fillvalue=None):   #function to make chunks of lists from itertools recipes book
     "Collect data into fixed-length chunks or blocks"
     # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
     args = [iter(iterable)] * n
     return it.zip_longest(*args, fillvalue="    ")
 
+
+def READ(n):                                #function to execute GD command
+    n = str(n)
+    temp1 = int(n[0])
+    temp2 = int(n[1])
+    if(temp2 == 0):
+        buffer = "".join(str_instr[temp1][0:9])
+
+def WRITE(n):                               #function to execute PD command
+    n = str(n)
+    temp1 = int(n[0])
+    temp2 = int(n[1])
+    if(temp2 == 0):
+        output = "".join(str_instr[temp1][0:9])
+    output_text.write(output)
+
+def TERMINATE():                            #function to terminate current program
+    pass
+
+def MOS(n, start_var):                      #Master function
+    if(n == 1):
+        READ(start_var)
+    elif(n == 2):
+        WRITE(start_var)
+    elif(n == 3):
+        # TERMINATE()
+        pass
+
+def EXECUTEUSERPROGRAM(user_prog):          #Slave mode functions to implement all operations
+    IC = 0
+    IR = ''
+    user_prog_4 = [user_prog[i:i+4] for i in range(0, len(user_prog), 4)]
+    for x in user_prog_4:
+        IR = x[0:2]
+        start_var = x[2:]
+        i = int(x[2])
+        k = int(x[3])
+        IC = IC + 1
+        if IR == 'LR':
+            R = "".join(str_instr[i][k])
+
+        elif IR == 'SR':
+            str_instr[i][k] = R
+
+        elif IR == 'CR':
+            if R == str_instr[i][k]:
+                C = 1
+            else:
+                C = 0
+
+        elif IR == 'BT':
+            if C == 1:
+                IC = start_var
+
+        elif IR == 'GD':
+            SI = 1
+            MOS(SI, start_var)
+
+        elif IR == 'PD':
+            SI = 2
+            MOS(SI, start_var)
+
+        elif IR == 'H ':
+            SI = 3
+            MOS(SI, start_var = 0)
+
+        else:
+            print("Something went wrong")         
+
+
 pp = pprint.PrettyPrinter(indent=4)     #initializing pprint function
-input_text = open("/home/kunal/Documents/Assignments/Sem2/OS/course_project/input1.txt", "r")   #file read from full path
-# input_text = open("input1.txt", "r")
-output_text = open("output1.txt", "w")
+# input & output files initialized
+input_text = open("input1.txt", "r")  
+output_text = open("output1.txt", "a")
 section_divide = input_text.read().split('$AMJ') #Split the sections according to the common beginning($AMJ)
-prog = '$AMJ'+section_divide[1] #changed from [2] to [1] as interpreter throws index error 
-# output_text.write(prog)
 
-# print(section_divide[1])
+for x in section_divide:                    #Loop to load and execute each program, one at a time
 
-instr_set = []
-remove_nl = []
+    card = card + 1    
+    prog = '$AMJ'+section_divide[card]
 
-for x in prog:
-    remove_nl.append(x)
-for x in remove_nl:
-    if(x == '\n'):
-        remove_nl.remove(x)
+    i_need_till_H = []
+    for x in prog:
+        if x == 'H':
+            break
+        i_need_till_H.append(x)
 
-for x in remove_nl[16:]:
-    if(x == 'H'):
-        break
-    else:
-        instr_set.append(x)
+    i_need_till_H = [x.replace('\n', '') for x in i_need_till_H]
+    i_need_till_H = "".join(i_need_till_H)
 
+    #This will give you data after H
+    after_H = prog.split('$DTA')[1].split('$')[0]
+    after_H = ''.join(after_H)
+    after_H = after_H.split("\n")
 
+    instr_set = []
+    remove_nl = []
 
-str_instr = ''.join(instr_set)
+    for x in prog:
+        remove_nl.append(x)
+    for x in remove_nl:
+        if(x == '\n'):
+            remove_nl.remove(x)
 
-data = ''.join(remove_nl[16:]).split('$DTA')[1].split('$')[0] #This will get the data inside the $DTA and $END section
+    for x in remove_nl[16:]:
+        if(x == 'H'):
+            break
+        else:
+            instr_set.append(x)
 
-# instr_data_4 = [instr_data[i:i+4] for i in range(0, len(instr_data), 4)]  #To divide the instruction and data set into 4 equal parts
-# creating seperate lists for instruction set and data.
-str_instr_4 = [str_instr[i:i+4] for i in range(0, len(str_instr), 4)]
-data_4 = [data[i:i+4] for i in range(0, len(data), 4)]
+    instr_set.append('H   ')
+    str_instr = ''.join(instr_set)
 
-# using grouper function to create uniform chunks
-part_1 = list(grouper(str_instr_4, 10))
-part_2 = list(grouper(data_4, 10))
+    user_prog = str_instr
 
-# instr_data_4 = str_instr_4 + data_4
-# puting them together to create final stack
-instr_data_4 = part_1 + part_2  
-pp.pprint(instr_data_4)
+    data = ''.join(remove_nl[16:]).split('$DTA')[1].split('$')[0] #This will get the data inside the $DTA and $END section
 
-# execution of program will start here
+    str_instr_4 = [str_instr[i:i+4] for i in range(0, len(str_instr), 4)]
+
+    str_instr = list(grouper(str_instr_4, 10))
+    for x in after_H[1:-1]:
+        x = [x[i:i+4] for i in range(0, len(x), 4)]
+        x = list(grouper(x, 10))
+        str_instr.extend(x)
+
+    empty = '        '
+    str_empty_4 = [empty[i:i+4] for i in range(0,len(empty), 4)]
+    while len(str_instr) != 10:
+        x = list(grouper(str_empty_4,10))
+        str_instr.extend(x)
+
+    # execution of program will start here
+    EXECUTEUSERPROGRAM(user_prog)
